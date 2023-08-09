8f72b25746a0aa88137c277185783ae1d3f2f604
hayato94087 <hayato94087@gmail.com>
2023-08-09 09:43:26 +0900

    デモコンポーネントの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { ProgressDemo } from "@/components/progress-component";
+import { TabsDemo } from "@/components/tabs-component";
+
 export default function Home() {
   return (
-    <main className="text-lg">
-      テストページ
+    <main className="grid grid-cols-2 gap-3 items-center justify-items-center mt-3">
+      <TabsDemo />
+      <ProgressDemo />
     </main>
-  )
+  );
 }
